ba60d7d70745990bb277a9ecb434d3ca9f72f1fb
cathyprime <magdalena.mjaroszynska@gmail.com>
2025-01-04 01:35:12 +0100

    Encode message

--- acorn-lsp.cpp
+++ acorn-lsp.cpp
@@ -1,7 +1,19 @@
-﻿#include <iostream>
+﻿#include <format>
+#include <iostream>
+#include <sstream>
+
+// interface ResponseError {
+//  code: integer;
+//  message: string;
+// }
+
+std::string make_message(const std::string& message) {
+    return std::format("Content-Length:{}\r\n\r\n{}", message.size(), message);
+}
 
 int main() {
-    std::cout << "Hello! ^-^!\n";
+    std::cout << make_message("{\"params\":{\"code\":420,\"message\":\"meow\"}}") << '\n';
+    // send on stdout
 
-    return 0;
+    return 420;
 }
